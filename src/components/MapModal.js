733936846a07ec3ae65986a6196569dbf77af2bb
Zhitao Geng <zhtgeng@gmail.com>
2024-10-29 00:35:56 -0700

    fix map pic

--- src/components/MapModal.js
+++ src/components/MapModal.js
@@ -49,7 +49,7 @@ export default function MapModal({ mapLocation }) {
                   <path strokeWidth="1" stroke="currentColor" d="M19.071 3.429h.001c3.905 3.905 3.905 10.237 0 14.142l-5.403 5.403a2.36 2.36 0 0 1-3.336 0l-5.375-5.375-.028-.028c-3.905-3.905-3.905-10.237 0-14.142 3.904-3.905 10.236-3.905 14.141 0ZM5.99 4.489v.001a8.5 8.5 0 0 0 0 12.02l.023.024.002.002 5.378 5.378a.859.859 0 0 0 1.214 0l5.403-5.404a8.5 8.5 0 0 0-.043-11.977A8.5 8.5 0 0 0 5.99 4.489Z"></path>
                 </svg>
               </div>
-              <img src={ autoLang({ zh: "/images/map-zh.png", en: "/images/map-en.png" })} className="img-fluid" alt="Map" />
+              <img src={ autoLang({ zh: "/images/map-zh.png", en: "images/map-en.png" })} className="img-fluid" alt="Map" />
             </div>
           </div>
         </div>
